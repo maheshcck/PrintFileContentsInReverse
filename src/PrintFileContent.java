61d00412f284f574d48cc896874038b95a3e50e9
maheshcck <mahesh.cck@gmail.com>
2015-04-21 23:00:31 -0400

    Initial commit

---
+++ src/PrintFileContent.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/*The application prints the characters from the files in the opposite order they appear.*/
+
+public class PrintFileContent {
+
+	public static void main(String[] args) {
+
+		String currLine;
+		BufferedReader reader;
+		List<String> fileLines;
+		
+		for (int i = args.length - 1; i >= 0; i--) {
+			try {
+				reader = new BufferedReader(new FileReader("src/" + args[i]));
+				// read all the lines into list
+				fileLines = new ArrayList<String>();
+				while ((currLine = reader.readLine()) != null) {
+					fileLines.add(currLine);
+				}
+				// print the lines in the reverse order
+				for (int j = fileLines.size() - 1; j >= 0; j--) {
+					//using the StringBuilder reverse function to reverse a String 
+					System.out.println(new StringBuilder(fileLines.get(j)).reverse().toString());
+				}
+
+			} catch (IOException e) {
+				// prints exception message if exception is caught
+				System.out.println(e.getMessage());
+			}
+
+		}
+	}
+
+}
